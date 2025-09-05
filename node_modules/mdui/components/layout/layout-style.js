553fa92af4fbea563e3fe4aa3a02a5a4606e0a16
vinci-sanchez <vincili550@gmail.com>
2025-09-05 18:36:55 +0800

    初始

---
+++ node_modules/mdui/components/layout/layout-style.js
@@ -0,0 +1,2 @@
+import { css } from 'lit';
+export const layoutStyle = css `:host{position:relative;display:flex;flex:1 1 auto;overflow:hidden}:host([full-height]:not([full-height=false i])){height:100%}`;
