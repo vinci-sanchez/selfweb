dab589d41e07fe79abae6b7e5ec97f86e9f9aa72
vinci-sanchez <vincili550@gmail.com>
2025-09-08 00:29:47 +0800

    添加了基本样式

---
+++ index.js
@@ -0,0 +1,14 @@
+import "mdui/mdui.css";
+import "mdui";
+// import "mdui/components/button.js";
+// import 'mdui/components/button-icon.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "axios";
+import "cors";
+//import 'express';
+import '@mdui/icons/search.js';
+import '@mdui/icons/123.js';
+import 'mdui/components/icon.js';
+
+
