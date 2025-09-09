449e7f20baa7865d81f683c03f6a325b09007e8b
vinci-sanchez <vincili550@gmail.com>
2025-09-09 23:46:44 +0800

    初具人样

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import "mdui/mdui.css";
 import "mdui";
-// import "mdui/components/button.js";
-// import 'mdui/components/button-icon.js';
+import "mdui/components/button.js";
+import 'mdui/components/button-icon.js';
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "axios";
-import "cors";
+// import "axios";
+// import "cors";
 //import 'express';
 import '@mdui/icons/search.js';
 import '@mdui/icons/123.js';
